3fee62248a874b7c60220acde2fce776ea37c857
Febriands <febrian.d.s@gmail.com>
2019-10-18 04:13:04 +0000

    add reqs.txt-2

--- facerog/wsgi.py
+++ facerog/wsgi.py
@@ -7,10 +7,32 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facerog.settings')
+
+# application = get_wsgi_application()
+
+import os 
+import time 
+import traceback 
+import signal 
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/home/febriands/public_html/facerog') 
+sys.path.append('/home/febriands/virtualenv/facerog_env/lib/python3.6/site-packages')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facerog.settings')
 
-application = get_wsgi_application()
+try: 
+    application = get_wsgi_application() 
+except Exception: 
+    # Error loading applications 
+    if 'mod_wsgi' in sys.modules: 
+        traceback.print_exc() 
+        os.kill(os.getpid(), signal.SIGINT) 
+        time.sleep(2.5)
